c02b9acbba5264fda86b226dbcda2ccb021a6dda
Federico Simone <91753433+Fedesin@users.noreply.github.com>
2023-03-29 19:22:06 -0300

    Update ControladorSaludo.java

--- TP1/ej7/webServer/src/main/java/com/example/ControladorSaludo.java
+++ TP1/ej7/webServer/src/main/java/com/example/ControladorSaludo.java
@@ -23,6 +23,7 @@
  * THE SOFTWARE.
  * =====LICENSE-END=====
  */
+package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
