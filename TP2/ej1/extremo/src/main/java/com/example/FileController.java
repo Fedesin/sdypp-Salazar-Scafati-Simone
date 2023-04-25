6602a2467880927bee6a55937f3f18de04685d08
Federico Simone <fedesimone31@gmail.com>
2023-04-25 16:58:10 -0300

    borre a la mierda maestro.java y extremo.java

--- TP2/ej1/extremo/src/main/java/com/example/FileController.java
+++ TP2/ej1/extremo/src/main/java/com/example/FileController.java
@@ -30,7 +30,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.example.Maestro;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
--- TP2/ej1/maestro/src/main/java/com/example/MaestroController.java
+++ TP2/ej1/maestro/src/main/java/com/example/MaestroController.java
@@ -30,7 +30,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.Extremo;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.io.IOException;
 import org.springframework.http.HttpStatus;
