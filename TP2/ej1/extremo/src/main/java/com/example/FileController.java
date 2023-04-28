57991c4998f4e92b307d9ad693157b01cd44b2ef
Federico Simone <fedesimone31@gmail.com>
2023-04-28 16:22:27 -0300

    arreglado el descargar, probar en profundidad

--- TP2/ej1/extremo/src/main/java/com/example/FileController.java
+++ TP2/ej1/extremo/src/main/java/com/example/FileController.java
@@ -77,71 +77,47 @@ public class FileController {
     */
     //id usuario de quien soy yo, host y puerto del extremo a conectarme, y filename del nombre del archivo a descargar
     @GetMapping("/descargar")
-public ResponseEntity<String> descargar(@RequestParam("filename") String filename, @RequestParam("host") String host, @RequestParam("port") int port, @RequestParam("id_usuario") int id_usuario){
-    try {
-        // Construir URL para consultar al servidor maestro si el archivo existe
-        String maestroUrl = "http://" + host + ":" + port + "/maestro/consultar/" + filename;
-        
-        // Crear un objeto RestTemplate para enviar solicitudes HTTP
-        RestTemplate restTemplate = new RestTemplate();
-        
-        // Crear un mapa de parámetros para enviar en la solicitud HTTP al servidor maestro
-        Map<String, Object> params = new HashMap<>();
-        params.put("filename", filename);
-        
-        // Convertir los parámetros a JSON
-        Gson gson = new Gson();
-        String json = gson.toJson(params);
-        
-        // Configurar encabezados de la solicitud HTTP
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        
-        // Crear una entidad HttpEntity con los parámetros y encabezados
-        HttpEntity<String> entity = new HttpEntity<>(json, headers);
-        
-        // Enviar una solicitud POST al servidor maestro para verificar si el archivo existe
-        // y obtener la dirección IP y el número de puerto del servidor que lo tiene
-        String respuesta = restTemplate.postForObject(maestroUrl, entity, String.class);
-        if (!respuesta.equals("404")){
-            // Si el archivo existe, construir una URL para descargarlo del servidor que lo tiene
-            String extremoUrl = "http://" + respuesta + "/getArchivo";
-            
-            // Crear otro objeto RestTemplate para enviar solicitudes HTTP al servidor que tiene el archivo
-            RestTemplate restTemplate2 = new RestTemplate();
-            
-            // Crear un mapa de parámetros para enviar en la solicitud HTTP al servidor que tiene el archivo
-            Map<String, Object> params2 = new HashMap<>();
-            params2.put("filename", filename);
-            
-            // Enviar una solicitud POST al servidor que tiene el archivo para descargarlo
-            System.out.println(extremoUrl+"BANDERAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-            ResponseEntity<byte[]> response = restTemplate2.postForEntity(extremoUrl, entity, byte[].class);
-            System.out.println("2BANDERAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    public ResponseEntity<String> descargar(@RequestParam("filename") String filename, @RequestParam("host") String host, @RequestParam("port") int port, @RequestParam("id_usuario") int id_usuario){
+        try {
+            // Construir URL para consultar al servidor maestro si el archivo existe
+            String maestroUrl = "http://" + host + ":" + port + "/maestro/consultar/" + filename;
+            // Crear un objeto RestTemplate para enviar solicitudes HTTP
+            RestTemplate restTemplate = new RestTemplate();
+            // Enviar una solicitud GET al servidor maestro para verificar si el archivo existe
+            // y obtener la dirección IP y el número de puerto del servidor que lo tiene
+            String respuesta = restTemplate.getForObject(maestroUrl, String.class);
+            if (!respuesta.equals("404")){
+                // Si el archivo existe, construir una URL para descargarlo del servidor que lo tiene
+                String extremoUrl = "http://" + respuesta + "/getArchivo/?nombre="+filename;
+                // Enviar una solicitud GET al servidor que tiene el archivo para descargarlo
+                System.out.println(extremoUrl);
+                // Crear otro objeto RestTemplate para enviar solicitudes HTTP al servidor que tiene el archivo
+                RestTemplate restTemplate2 = new RestTemplate();   
+                ResponseEntity<byte[]> response = restTemplate2.getForEntity(extremoUrl, byte[].class);
 
-            // Guardar el archivo descargado como un objeto MultipartFile
-            MultipartFile archivo = new MockMultipartFile("file", response.getBody());
-            
-            // Obtener la dirección IP local y el número de puerto del servidor
-            InetAddress ipLocal = InetAddress.getLocalHost();
-            String hostExtremo = ipLocal.getHostAddress();
-            int portLocal = Integer.parseInt(env.getProperty("server.port"));
-            
-            // Llamar al método uploadFile para cargar el archivo descargado en el servidor
-            uploadFile(archivo,hostExtremo,portLocal,id_usuario);
-            
-            // Devolver una respuesta HTTP 200 (OK) con un mensaje indicando que se descargó el archivo correctamente
-            return ResponseEntity.ok("Archivo descargado ");
-        } else {
-            // Si el archivo no existe, devolver una respuesta HTTP 404 (NOT FOUND)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+                // Guardar el archivo descargado como un objeto MultipartFile
+                MultipartFile archivo = new MockMultipartFile("file", response.getBody());
+                
+                // Obtener la dirección IP local y el número de puerto del servidor
+                InetAddress ipLocal = InetAddress.getLocalHost();
+                String hostExtremo = ipLocal.getHostAddress();
+                int portLocal = Integer.parseInt(env.getProperty("server.port"));
+                
+                // Llamar al método uploadFile para cargar el archivo descargado en el servidor
+                uploadFile(archivo,hostExtremo,portLocal,id_usuario);
+                
+                // Devolver una respuesta HTTP 200 (OK) con un mensaje indicando que se descargó el archivo correctamente
+                return ResponseEntity.ok("Archivo descargado ");
+            } else {
+                // Si el archivo no existe, devolver una respuesta HTTP 404 (NOT FOUND)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+        } catch (IOException e) {
+            // Si se produce una excepción al enviar o recibir la solicitud HTTP, devolver una respuesta HTTP 500 (INTERNAL SERVER ERROR)
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-    } catch (IOException e) {
-        // Si se produce una excepción al enviar o recibir la solicitud HTTP, devolver una respuesta HTTP 500 (INTERNAL SERVER ERROR)
-        e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
-}
 /* 
     @GetMapping("/GET ArchivosDisponibles")
     public ResponseEntity<Resource> ArchivosDisponibles() {
